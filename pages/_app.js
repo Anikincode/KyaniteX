9549c047d4e4ca1f7b41ab203d3d904b7eede430
Nicholas Goorwah <Nick@VolterraTechnology.com>
2022-05-30 08:26:57 -0400

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -53,7 +53,7 @@ function App({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>Next.js 11 - User Registration and Login Example</title>
+                <title>KyaniteX Login and Register</title>
                 
                 {/* eslint-disable-next-line @next/next/no-css-tags */}
                 <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
@@ -70,10 +70,10 @@ function App({ Component, pageProps }) {
             {/* credits */}
             <div className="text-center mt-4">
                 <p>
-                    <a href="https://jasonwatmore.com/post/2021/08/19/next-js-11-user-registration-and-login-tutorial-with-example-app" target="_top">Next.js 11 - User Registration and Login Tutorial with Example App</a>
+                    <a href="" target="_top">KyaniteX - Web Master - Login & Register</a>
                 </p>
                 <p>
-                    <a href="https://jasonwatmore.com" target="_top">JasonWatmore.com</a>
+                    <a href="" target="_top"></a>
                 </p>
             </div>
         </>
